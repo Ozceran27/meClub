974e5216f3db2a059570becefbbc3bc47cd9b98c
Fernando <64290756+Ozceran27@users.noreply.github.com>
2025-09-05 05:19:55 -0300

    Load env before server setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3006;
-require('dotenv').config();
 const PasswordResetsModel = require('./models/passwordResets.model');
 
 // MIDDLEWARES ------------------------------------------------------------------------------------
