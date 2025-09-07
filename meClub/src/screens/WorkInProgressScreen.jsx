497969376088654de99d7871a56edb6b2971ba90
Ozceran <fernandoariel27@gmail.com>
2025-09-07 01:58:17 -0300

    Negan- 0.2.7925.0158

--- meClub/src/screens/WorkInProgressScreen.jsx
+++ meClub/src/screens/WorkInProgressScreen.jsx
@@ -3,12 +3,7 @@ import { View, Text, Image } from 'react-native';
 export default function WorkInProgressScreen() {
   return (
     <View className="flex-1 bg-mc-bg items-center justify-center">
-      <Image
-        source={require('../../assets/icon.png')}
-        className="w-24 h-24 mb-4"
-        resizeMode="contain"
-      />
-      <Text className="text-mc-text text-xl">Estamos trabajando</Text>
+          <Text className="text-mc-text text-xl">Estamos trabajando</Text>
     </View>
   );
 }
