449eefff4f4257616983d05d4f9a47a4085ade4a
Ozceran <fernandoariel27@gmail.com>
2025-11-26 03:16:10 -0300

    Hermes- 0.1.261125.0316

--- backend/routes/messages.routes.js
+++ backend/routes/messages.routes.js
@@ -5,6 +5,7 @@ const { requireRole } = require('../middleware/roles.middleware');
 const messagesController = require('../controllers/messages.controller');
 
 router.get('/', verifyToken, messagesController.listInbox);
+router.get('/inbox', verifyToken, messagesController.listInbox);
 router.post('/', verifyToken, requireRole('club'), messagesController.createMessage);
 router.patch('/inbox/:inboxId/leer', verifyToken, messagesController.markAsRead);
 router.delete('/inbox/:inboxId', verifyToken, messagesController.deleteInbox);
