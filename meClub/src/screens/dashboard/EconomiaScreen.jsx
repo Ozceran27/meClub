d62d467a6521fcad9910d1bec9ac98d886ff45fc
Ozceran <fernandoariel27@gmail.com>
2025-11-27 02:44:43 -0300

    Emp- 0.1.271125.0244

--- meClub/src/screens/dashboard/EconomiaScreen.jsx
+++ meClub/src/screens/dashboard/EconomiaScreen.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useQuery, useQueryClient, queryOptions } from "@tanstack/react-query"
+import { getClubSummary, api } from "../../lib/api"
+import { useAuth } from '../../features/auth/useAuth';
+
+
+
+async function getUser() {
+  //llama a un endpoint del backend que le devuelve la información del usuario
+  const data = await api.get("/usuarios/perfil") // endpoint del back
+  return data.usuario
+}
+
+const getClubSummaryQueryOptions = (user) => queryOptions({
+  queryKey: ["summary", user],
+  queryFn: async () => await getClubSummary({ clubId: user.clubId })
+})
 
 export default function EconomiaScreen({ summary = {} }) {
-  const formattedAmount = React.useMemo(
-    () => `$${Number(summary.economiaMes ?? 0).toLocaleString('es-AR')}`,
-    [summary.economiaMes],
-  );
+  const { user } = useAuth()
+
+  const { data, isPending, error } = useQuery({
+    ...getClubSummaryQueryOptions(user), // { clubId: 2 }
+    select: (data) => `$${Number(data?.economiaMes ?? 0).toLocaleString('es-AR')}`,
+  });
 
   return (
     <View className="flex-1">
@@ -18,7 +36,9 @@ export default function EconomiaScreen({ summary = {} }) {
           <Text className="text-white/70 text-[14px] uppercase tracking-[0.2em]">
             Ingresos del mes
           </Text>
-          <Text className="text-white text-[40px] font-extrabold mt-3">{formattedAmount}</Text>
+          {!error 
+            ? <Text className="text-white text-[40px] font-extrabold mt-3">{isPending ? "Loading..." : data}</Text> 
+            : <Text className="text-white text-[40px] font-extrabold mt-3">OCURRIO UN ERROR</Text>}
           <Text className="text-white/50 mt-2">
             Mantén al día tus registros económicos y controla la salud financiera del club.
           </Text>
